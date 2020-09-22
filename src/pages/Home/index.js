69e43f206f38c32508a6a8501d0bb057d814e5b8
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-09-21 23:39:08 -0300

    homenagem bianca

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+import bianca from "./foto/bianca.jpg";
+
+import "./styles.css";
+
 function Home() {
   return (
-    <div>
+    <div className="container">
       <h1>Oiê Biancaaa</h1>
+      <img width="100px" src={bianca} className="bianca" />
     </div>
   );
 }
