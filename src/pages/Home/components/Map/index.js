99966f5644e1add0228100bac950186d3443f33e
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-09-23 15:48:07 -0300

    Creating component map

---
+++ src/pages/Home/components/Map/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+
+function Map() {
+  return (
+    <div className="map">
+      <h1>I am a map</h1>
+    </div>
+  );
+}
+
+export default Map;
