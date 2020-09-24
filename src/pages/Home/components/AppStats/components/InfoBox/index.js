145e7895eac45df922891af7fab8a80c76822c34
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-09-24 14:27:56 -0300

    Consertando error de escrita

--- src/pages/Home/components/AppStats/components/InfoBox/index.js
+++ src/pages/Home/components/AppStats/components/InfoBox/index.js
@@ -10,7 +10,7 @@ function InfoBox({ title, cases, total }) {
           <Typography className="infoBox_title" color="textSecondary">
             {title}
           </Typography>
-          <h2 className="infoBox_cases">{cases} Hoje</h2>
+          <h2 className="infoBox_cases">{cases}</h2>
           <Typography className="infoBox_total" color="textSecondary">
             {total} Total
           </Typography>
