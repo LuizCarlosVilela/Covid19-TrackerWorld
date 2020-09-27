0604e4493f29ad723bba397c6d62fe0eebdd6778
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-09-26 22:00:52 -0300

    Update styles

--- src/pages/Home/components/AppStats/components/InfoBox/index.js
+++ src/pages/Home/components/AppStats/components/InfoBox/index.js
@@ -1,22 +1,29 @@
 import React from "react";
-
 import { Card, CardContent, Typography } from "@material-ui/core";
+import "./styles.css";
 
-function InfoBox({ title, cases, total }) {
+function InfoBox({ title, cases, total, active, isRed, ...props }) {
+  console.log(title, active);
   return (
-    <div>
-      <Card className="infoBox">
-        <CardContent>
-          <Typography className="infoBox_title" color="textSecondary">
-            {title}
-          </Typography>
-          <h2 className="infoBox_cases">{cases}</h2>
-          <Typography className="infoBox_total" color="textSecondary">
-            {total} Total
-          </Typography>
-        </CardContent>
-      </Card>
-    </div>
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && "infoBox--selected"} ${
+        isRed && "infoBox--red"
+      }`}
+    >
+      <CardContent>
+        <Typography color="primary" gutterBottom>
+          {title}
+        </Typography>
+        <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>
+          {cases}
+        </h2>
+
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
 
